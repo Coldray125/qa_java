e6c6b12aa3e231d4649029e43e71207a6ff541ba
Artem Evlakhov <Artem.Evlahov@leroymerlin.ru>
2022-05-15 12:58:56 +0300

    Revert "Test"
    This reverts commit 3ae4cb726afc9c320df8778cd7373f13f664b503.

--- src/main/java/com/example/Animal.java
+++ src/main/java/com/example/Animal.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.List;
 
 public class Animal {
-//take a look around
+
     public List<String> getFood(String animalKind) throws Exception {
         if ("Травоядное".equals(animalKind)) {
             return List.of("Трава", "Различные растения");
